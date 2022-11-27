4c4fcd62c7114ae58139fa3a7c0723202ad47915
evangel0s <vaggelis817@gmail.com>
2022-11-27 15:55:14 +0200

    This is and a example

---
+++ MyScript.js
@@ -0,0 +1,13 @@
+let rock;
+let paper;
+let scissors; 
+
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection=rock || computerSelection= paper){
+    return playerSelection= 'wins;
+    } else if_lkl.
+
+    
+
+
