0db974de3022444f023e4a2e2a747382edb317bf
evangel0s <vaggelis817@gmail.com>
2022-12-19 00:10:16 +0200

    hey

--- MyScript.js
+++ MyScript.js
@@ -1,13 +1,70 @@
-let rock;
-let paper;
-let scissors; 
+ const computerChoiceDisplay = document.getElementById('computer-choice')
+ const userChoiceDisplay = document.getElementById('user-choice')
+ const resultDisplay  = document.getElementById('result')
+ const possibleChoices = document.querySelectorAll('button')
+ let userSelection
+ let computerSelection
+ let result
+  
+ 
+ possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click',(e)=>{
+    userSelection=e.target.id
+    userChoiceDisplay.innerHTML= userSelection 
+    generateComputerChoice()
+    playRound()
+ }))
+ function generateComputerChoice(){
+const randomNumber = Math.floor(Math.random()*possibleChoices.length)+1;
 
-
-function playRound(playerSelection,computerSelection){
-    if (playerSelection=rock || computerSelection= paper){
-    return playerSelection= 'wins;
-    } else if_lkl.
-
+if (randomNumber === 1) {
+    computerSelection = 'rock'
+}
+if (randomNumber === 2) {
+    computerSelection ='paper'
+}
+if (randomNumber === 3) {
+    computerSelection ='scissors'
+}
+ computerChoiceDisplay.innerHTML = computerSelection
+ }
+ 
+  
+  
+ function playRound(){
+      
+    if ((userSelection === 'rock' && computerSelection === 'scissors') ||
+        (userSelection === 'scissors' && computerSelection === 'paper')||
+        (userSelection === 'paper' && computerSelection === 'rock')){
+       
+      result = ('You Win');
+      }  
+     else if (userSelection === computerSelection){  
+     result = ('Its a tie');
+     }
+     
+     else if ((userSelection === 'rock' && computerSelection === 'paper') ||
+     (userSelection === 'scissors' && computerSelection === 'rock')||
+     (userSelection === 'paper' && computerSelection === 'scissors')) {
+         
+     result = ('you lose');
+     }
+     resultDisplay.innerHTML =result
+    }
     
+     
 
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
